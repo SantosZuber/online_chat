e79bfe00954df31e4b7c4b620cc78fa608514d0a
Santos Zuberbuhler <santoszuberbuhler2003@protonmail.com>
2022-09-21 16:35:58 -0300

    working

--- index.js
+++ index.js
@@ -6,7 +6,39 @@ const app = express();
 const httpServer = HTTPServer(app);
 const io = new SocketServer(httpServer);
 
+function validateUsername(data) {
+  if (data.includes("<", ">", "*", "/")) {
+    data = "Hacker";
+  } else if (data.includes(" ")) {
+    data = "Hacker";
+  } else if (data === null) {
+    data = "Hacker";
+  }
+}
+function validateMsg(data) {
+  if (data.includes("<", ">", "*", "/")) {
+    data = "There was an error on this message";
+  } else if (data === null) {
+    data = "There was an error on this message";
+  }
+}
+
 app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  const mensajes = [];
+  console.log("Cliente conectado: ", socket.id);
+  const colors = ["purple", "green", "blue"];
+  const randomNum = Math.floor(Math.random() * 3);
+  console.log(randomNum)
+  const color = colors[randomNum];
+  socket.on("message", (username, message) => {
+    validateUsername(username);
+    validateMsg(message);
+    mensajes.push({ username: username, message: message, color: color });
+    io.sockets.emit("newmsg", mensajes);
+  });
+});
 
 httpServer.listen(8080, () => {
   console.log("Server open, port: ", 8080);
--- public/app.js
+++ public/app.js
@@ -1,1 +1,85 @@
-io().connect
+const socket = io.connect();
+let chat = document.getElementById("chat");
+let username;
+document.getElementById("welcome").style.display = "none";
+document.getElementById("chat-page").style.display = "none";
+
+document.getElementById("form").onsubmit = function (e) {
+  e.preventDefault();
+  if (!document.getElementById("username").value == "") {
+    username = document.getElementById("username").value;
+    validateUsername(username);
+    document.getElementById("login").style.display = "none";
+    document.getElementById("welcome").style.display = "flex";
+    document.getElementById("welcome-title").innerHTML = `Hello ${username}`;
+
+    //Mejorable con una promesa probablemente ->
+    setTimeout(() => {
+      document.getElementById("welcome").style.animationName = "dissapear";
+      document.getElementById("welcome").style.animationDuration = "1s";
+      document.getElementById("welcome").style.animationIterationCount =
+        "initial";
+      document.getElementById("welcome").style.animationTimingFunction =
+        "ease-in";
+    }, 2000);
+    setTimeout(() => {
+      document.getElementById("chat-page").style.display = "flex";
+    }, 2000);
+    setTimeout(() => {
+      document.getElementById("welcome").style.display = "none";
+    }, 2999);
+  }
+};
+
+document.getElementById("input-container").onsubmit = function (e) {
+  e.preventDefault();
+  if (!document.getElementById("message").value == "") {
+    let message = document.getElementById("message").value;
+    validateMsg(message);
+    socket.emit("message", username, message);
+    document.getElementById("message").value = "";
+    message = "";
+  }
+  chat.scrollBy(0, chat.scrollHeight);
+};
+
+socket.on("newmsg", (data) => {
+  chat.innerHTML = "";
+  data.forEach((e) => {
+    let date = new Date();
+
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let amOrPm = hrs >= 12 ? "pm" : "am";
+
+    const { username, message, color } = e;
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("message");
+    messageElement.innerHTML = `<p class="${color}">${username}</p> <p class="msg-text">${message}</p><span>${hrs}:${mins}${amOrPm}</span>`;
+    chat.appendChild(messageElement);
+    return;
+  });
+});
+
+function validateUsername(data) {
+  if (data.includes("<", ">", "*", "/")) {
+    data = "";
+    alert("Characters like < > * / are not allowed");
+    document.location.reload();
+  } else if (data.includes(" ")) {
+    data = "";
+    alert("Spaces are not allowed");
+    document.location.reload();
+  } else if (data === null) {
+    data = "";
+  }
+}
+function validateMsg(data) {
+  if (data.includes("<", ">", "*", "/")) {
+    data = "";
+    alert("Characters like < > * / are not allowed");
+    document.location.reload();
+  } else if (data === null) {
+    data = "";
+  }
+}
